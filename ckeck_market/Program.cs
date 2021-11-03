74139951d1d6a396d0013840dfbba3f3b23e23f7
Egor <deidarko@inbox.ru>
2021-11-03 23:14:50 +0300

    Добавьте файлы проекта.

---
+++ ckeck_market/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json; 
+using System.Timers;
+using System.Web;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Telegram.Bot;
+
+namespace ckeck_market
+{
+    class Program
+    {
+        static Int64 chanall_id = 401036948;
+        static TelegramBotClient Bot_ntf;
+        static void Main(string[] args)
+        {
+            string Bot_ntf_api = "1866819612:AAHKMvSXhcD3tuwub6L07Esp2rn1ExcSbYU";
+            Bot_ntf = new TelegramBotClient(Bot_ntf_api);
+
+              txt_horse();
+        }
+
+
+        static void kaka_nft()
+        {
+            while (true)
+            {
+                try
+                {
+                    var url = "https://www.binance.com/bapi/nft/v1/friendly/nft/artist-product-list";
+
+                    var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+                    httpRequest.Method = "POST";
+                    httpRequest.Accept = "application/json";
+                    httpRequest.Headers["clienttype"] = "web";
+                    httpRequest.ContentType = "application/json";
+
+                    var data = "{\"reSale\":\"\",\"tradeType\":0,\"currency\":\"\",\"amountFrom\":\"\",\"amountTo\":\"\",\"keyword\":\"\",\"orderBy\":\"list_time\",\"orderType\":-1,\"page\":1,\"rows\":100,\"creatorId\":\"49230611\"}";
+
+                    using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+                    {
+                        streamWriter.Write(data);
+                    }
+
+                    var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+                    string result = null;
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        result = streamReader.ReadToEnd();
+                    }
+
+                    Console.WriteLine(httpResponse.StatusCode);
+                    var tar_price = JsonConvert.DeserializeObject<json.Root>(result);
+
+
+                    for (int i = 0; i < tar_price.Data.Rows.Count; i++)
+                    {
+                        if (Convert.ToInt32(tar_price.Data.Rows[i].Amount) < 500)
+                        {
+                            string url_out = "https://www.binance.com/ru/nft/goods/detail?productId=" + tar_price.Data.Rows[i].ProductId + "&isOpen=false&isProduct=1";
+
+                            Console.WriteLine(url_out);
+                            Bot_ntf.SendTextMessageAsync(chanall_id, url_out);
+
+                        }
+                    }
+
+                    Thread.Sleep(5000);
+                }
+                catch
+                {
+
+                }
+               
+            }
+        }
+        static void txt_horse()
+        {
+            List<string> product = new List<string>();
+            for (int page = 1; page < 10; page++)
+            {
+                var url = "https://www.binance.com/bapi/nft/v1/friendly/nft/layer-product-list";
+
+                var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+                httpRequest.Method = "POST";
+                httpRequest.Accept = "application/json";
+                httpRequest.Headers["clienttype"] = "web";
+                httpRequest.ContentType = "application/json";
+
+                var data = "{\"reSale\":\"\",\"tradeType\":0,\"currency\":\"BNB\",\"amountFrom\":\"\",\"amountTo\":\"\",\"keyword\":\"\",\"orderBy\":\"list_time\",\"orderType\":-1,\"page\":"+page+",\"rows\":100,\"collectionId\":\"507186726677291009\"}";
+
+                using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(data);
+                }
+
+                var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+                string result = null;
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    result = streamReader.ReadToEnd();
+                }
+
+                Console.WriteLine(httpResponse.StatusCode);
+                var tar_price = JsonConvert.DeserializeObject<json.Root>(result);
+
+
+                for (int i = 0; i < tar_price.Data.Rows.Count; i++)
+                {
+                    if (tar_price.Data.Rows[i].Amount.ToString() == "0.5")
+                    {
+
+                        product.Add(tar_price.Data.Rows[i].ProductId.ToString());
+
+                    }
+                }
+
+
+            }
+            File.WriteAllLines("test.txt", product);
+            var list = File.ReadLines("test.txt").ToList();
+        }
+    }
+}
---
+++ ckeck_market/json.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ckeck_market
+{
+    class json
+    {
+        // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class Owner
+        {
+            [JsonProperty("userId")]
+            public int UserId { get; set; }
+
+            [JsonProperty("avatarUrl")]
+            public string AvatarUrl { get; set; }
+
+            [JsonProperty("nickName")]
+            public string NickName { get; set; }
+
+            [JsonProperty("artist")]
+            public bool Artist { get; set; }
+        }
+
+        public class Creator
+        {
+            [JsonProperty("userId")]
+            public int UserId { get; set; }
+
+            [JsonProperty("avatarUrl")]
+            public string AvatarUrl { get; set; }
+
+            [JsonProperty("nickName")]
+            public string NickName { get; set; }
+
+            [JsonProperty("artist")]
+            public bool Artist { get; set; }
+        }
+
+        public class Approve
+        {
+            [JsonProperty("count")]
+            public int Count { get; set; }
+
+            
+        }
+
+        public class Row
+        {
+            [JsonProperty("productId")]
+            public string ProductId { get; set; }
+
+            [JsonProperty("title")]
+            public string Title { get; set; }
+
+            [JsonProperty("coverUrl")]
+            public string CoverUrl { get; set; }
+
+            [JsonProperty("tradeType")]
+            public int TradeType { get; set; }
+
+            [JsonProperty("nftType")]
+            public int NftType { get; set; }
+
+            [JsonProperty("amount")]
+            public string Amount { get; set; }
+
+            [JsonProperty("currency")]
+            public string Currency { get; set; }
+
+            [JsonProperty("setStartTime")]
+            public object SetStartTime { get; set; }
+
+            [JsonProperty("setEndTime")]
+            public object SetEndTime { get; set; }
+
+            [JsonProperty("timestamp")]
+            public object Timestamp { get; set; }
+
+            [JsonProperty("rarity")]
+            public int Rarity { get; set; }
+
+            [JsonProperty("status")]
+            public int Status { get; set; }
+
+            [JsonProperty("owner")]
+            public Owner Owner { get; set; }
+
+            [JsonProperty("creator")]
+            public Creator Creator { get; set; }
+
+            [JsonProperty("mediaType")]
+            public string MediaType { get; set; }
+
+            [JsonProperty("favorites")]
+            public int Favorites { get; set; }
+
+            [JsonProperty("network")]
+            public string Network { get; set; }
+
+            [JsonProperty("approve")]
+            public Approve Approve { get; set; }
+        }
+
+        public class Data
+        {
+            [JsonProperty("total")]
+            public int Total { get; set; }
+
+            [JsonProperty("rows")]
+            public List<Row> Rows { get; set; }
+        }
+
+        public class Root
+        {
+            [JsonProperty("code")]
+            public string Code { get; set; }
+
+            [JsonProperty("message")]
+            public object Message { get; set; }
+
+            [JsonProperty("messageDetail")]
+            public object MessageDetail { get; set; }
+
+            [JsonProperty("data")]
+            public Data Data { get; set; }
+
+            [JsonProperty("success")]
+            public bool Success { get; set; }
+        }
+
+
+
+
+
+    }
+}
